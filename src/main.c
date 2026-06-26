f32e5521590cdfb9344e9a7fb79b1b7dce008a4f
Qizlei <vasek.dan.2007@gmail.com>
2026-06-27 00:02:10 +0300

    Update main.c
    вывод массива

--- src/main.c
+++ src/main.c
@@ -1,11 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "sort.h"
-#include "file_io.h"
-#include "menu.h"
-
-int main() {
-    system("chcp 65001");
-    printf("Система инициализирована.\n");
-    return 0;
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
