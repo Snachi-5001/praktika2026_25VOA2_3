89edd6481f8391b631775b83ce03d59a67fb25e6
Qizlei <vasek.dan.2007@gmail.com>
2026-06-26 23:39:35 +0300

    Update main.c
    массив для проверки

--- src/main.c
+++ src/main.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "sort.h"
-#include "file_io.h"
-#include "menu.h"
-
 int main() {
-    system("chcp 65001");
-    printf("Система инициализирована.\n");
+    // Статический массив созданный заранее
+    int arr[] = { 64, 25, 12, 22, 11, 90, 3, 45, 18, 7 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("sortirovka viborom \n");
+    printf("ishoidni masiv: ");
+    printArray(arr, n);
+
+    sortirovka(arr, n);
+
+    printf("otsortirivani massiv: ");
+    printArray(arr, n);
     return 0;
 }
