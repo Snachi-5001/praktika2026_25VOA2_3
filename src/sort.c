096854bc296781bfa28d66f3c839e7006fa2f4bb
unknown <vasek.dan.2007@gmail.com>
2026-06-27 01:47:07 +0300

    a sorting algorithm has been created

--- src/sort.c
+++ src/sort.c
@@ -1,4 +1,22 @@
-#ifndef SORT_H
-#define SORT_H
-void shell_sort(int *arr, int n);
-#endif
+//Сортировка
+void sortirovka(int arr[], int n) {
+    int i, j, minIndex, temp;
+
+    for (i = 0; i < n - 1; i++) {
+        minIndex = i;
+
+        // Ищем минимальный элемент
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Сдвиг вправо для устойчивости
+        temp = arr[minIndex];
+        for (j = minIndex; j > i; j--) {
+            arr[j] = arr[j - 1];
+        }
+        arr[i] = temp;
+    }
+}
---
+++ src/sort.h
@@ -0,0 +1,7 @@
+// Вывод массива
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
