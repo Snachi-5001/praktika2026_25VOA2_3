404a23ef688065892ce1b49ded59310e1418596e
unknown <vasek.dan.2007@gmail.com>
2026-06-28 21:15:25 +0300

    dobavil function dla zamera vremeni i menu dla s randomaizerom

--- src/sort.c
+++ src/sort.c
@@ -1,29 +1,41 @@
-//Сортировка
 #include <stdio.h>
+#include <time.h>
+#include "sort.h"
+
 void sortirovka(int arr[], int n) {
     int i, j, minIndex, temp;
-
     for (i = 0; i < n - 1; i++) {
         minIndex = i;
-
-        // Ищем минимальный элемент
-        for (j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
+        for (j = i + 1; j < n; j++)
+            if (arr[j] < arr[minIndex])
                 minIndex = j;
-            }
-        }
-
-        // Сдвиг вправо для устойчивости
         temp = arr[minIndex];
-        for (j = minIndex; j > i; j--) {
+        for (j = minIndex; j > i; j--)
             arr[j] = arr[j - 1];
-        }
         arr[i] = temp;
     }
 }
+
 void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
-    }
     printf("\n");
+}
+
+double getSortTime(int arr[], int n) {
+    // Создаём копию массива, чтобы не портить оригинал
+    int* copy = (int*)malloc(n * sizeof(int));
+    if (copy == NULL) {
+        fprintf(stderr, "Memory allocation error in getSortTime\n");
+        return -1.0;
+    }
+    for (int i = 0; i < n; i++)
+        copy[i] = arr[i];
+
+    clock_t start = clock();
+    sortirovka(copy, n);
+    clock_t end = clock();
+
+    free(copy);
+    return (double)(end - start) / CLOCKS_PER_SEC;
 }
--- src/sort.h
+++ src/sort.h
@@ -1,3 +1,10 @@
-// Вывод массива
+#ifndef SORT_H
+#define SORT_H
+
+#include <time.h>
+
 void sortirovka(int arr[], int n);
 void printArray(int arr[], int n);
+double getSortTime(int arr[], int n);  // возвращает время в секундах
+
+#endif
